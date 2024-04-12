b511f556c4779ae3fb4dcce61aec17f050e6664b
Octávio <screminoctavio@gmail.com>
2024-04-11 23:02:10 -0300

    making navbar working

--- costs/src/components/pages/Company.js
+++ costs/src/components/pages/Company.js
@@ -1,5 +1,5 @@
 function Company() {
-    return <h1>Company</h1>
+    return <h1>Empresa</h1>
 }
 
 export default Company
--- costs/src/components/pages/NewProject.js
+++ costs/src/components/pages/NewProject.js
@@ -1,5 +1,5 @@
 function NewProject() {
-    return <h1>Company</h1>
+    return <h1>Novo projeto</h1>
 }
 
 export default NewProject

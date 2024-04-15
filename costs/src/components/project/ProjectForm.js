80bbd5c5fbf90541ba1735f74142c534312c9668
Octávio <screminoctavio@gmail.com>
2024-04-15 18:47:11 -0300

    first form done

--- costs/src/components/project/ProjectForm.js
+++ costs/src/components/project/ProjectForm.js
@@ -2,9 +2,22 @@
 function ProjectForm() {
     return (
     <form>
-        <input type >
+        <div>
+            <input type="text" placeholder="Insira o nome do projeto" />
+        </div>
+        <div>
+            <input type="number" placeholder="Insira o orçamento total" />
+        </div>
+        <div>
+            <select name="category_id">
+            <option disabled selected>Selecione a categoria</option>
+            </select>
+        </div>
+        <div>
+            <input type="submit" placeholder="Criar projeto" />
+        </div>
     </form>
-        )
+    )
 }
 
 export default ProjectForm 

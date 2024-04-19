530a9077919253616b60d63690df6f26e33abadb
Octávio <screminoctavio@gmail.com>
2024-04-18 22:41:42 -0300

    fixing the message

--- costs/src/components/pages/Projects.js
+++ costs/src/components/pages/Projects.js
@@ -3,7 +3,6 @@ import {useLocation} from 'react-router-dom'
 import Message from '../layout/Message'
 
 function Projects() {
-
     const location = useLocation()
     let message = ''
     if (location.state) {

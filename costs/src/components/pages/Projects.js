3c5a462b02d626d7c41c34a346949c3bdd55f525
Octávio <screminoctavio@gmail.com>
2024-04-19 22:01:10 -0300

    creating dashboard

--- costs/src/components/pages/Projects.js
+++ costs/src/components/pages/Projects.js
@@ -1,6 +1,10 @@
 import {useLocation} from 'react-router-dom'
 
 import Message from '../layout/Message'
+import Container from '../lauyout/Container'
+import LinkButton from '../layout/LinkButton'
+
+import styles from './Projects.module.css'
 
 function Projects() {
     const location = useLocation()
@@ -11,8 +15,14 @@ function Projects() {
 
     return (
     <div>
+        <div>
         <h1>Meus Projetos</h1>
+        <a href="#">Novo projeto</a>
+        </div>
         {message && <Message type="success" msg={message} />}
+        <Container customClass="start">
+            <p>Projetos...</p>
+        </Container>
     </div>
     )
 }

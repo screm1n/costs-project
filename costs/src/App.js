a6f272be42eb73fcb005c730836fbd0a95982059
Octávio <screminoctavio@gmail.com>
2024-06-11 20:44:01 -0300

    making "edit" button functional

--- costs/src/App.js
+++ costs/src/App.js
@@ -4,7 +4,7 @@ import Contact from './components/pages/Contact'
 import NewProject from './components/pages/NewProject'
 import Projects from './components/pages/Projects'
 import Company from './components/pages/Company'
-
+import Project from './components/pages/Project'
 import Container from './components/layout/Container'
 import Navbar from './components/layout/Navbar'
 import Footer from './components/layout/Footer'
@@ -15,11 +15,12 @@ function App() {
             <Navbar />
             <Container customClass="min-height">
             <Routes>
-                <Route exact path="/home" element={<Home />} />
-                <Route exact path="/projects" element={<Projects />} />
-                <Route exact path="/contact" element={<Contact />} />
-                <Route exact path="/company" element={<Company />} />
-                <Route exact path="/newproject" element={<NewProject />} />
+                <Route path="/home" element={<Home />} />
+                <Route path="/projects" element={<Projects />} />
+                <Route path="/contact" element={<Contact />} />
+                <Route path="/company" element={<Company />} />
+                <Route path="/newproject" element={<NewProject />} />
+                <Route path="/project/:id" element={<Project />} />
             </Routes>
             </Container>
             <Footer />

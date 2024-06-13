d3dc987336ff4595e2d87e4bec1816009bbb607b
Octávio <screminoctavio@gmail.com>
2024-06-12 21:29:21 -0300

    setting timeout and adding container to project

--- costs/src/components/pages/Project.js
+++ costs/src/components/pages/Project.js
@@ -3,14 +3,17 @@ import styles from './Project.module.css'
 import {useParams} from 'react-router-dom'
 import {useState, useEffect} from 'react'
 
+import Loading from '../layout/Loading'
+import Container from '../layout/Container'
+
 function Project() {
     const { id } = useParams()
 
     const [project, setProject] = useState([])
 
     useEffect(() => {
-        
-        fetch(`http://localhost:5000/projects/${id}`, {
+        setTimeout(() => {
+            fetch(`http://localhost:5000/projects/${id}`, {
             method: 'GET',
             headers: {
                 'Content-Type': 'application/json',
@@ -21,9 +24,19 @@ function Project() {
             setProject(data)
         })
         .catch(err => console.log)
+        }, 500)
     }, [id])
 
-    return <p>{project.name}</p>
+    return (
+    <>
+        {project.name ? (
+            <div>
+                <Container customClass="column"></Container>
+            </div>
+        ) : (
+         <Loading />
+        )}
+    </>
 }
 
 export default Project

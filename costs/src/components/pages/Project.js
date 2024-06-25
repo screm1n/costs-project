fa37ae881c96d666cd618ded019a8b4ed47b473b
Octávio <screminoctavio@gmail.com>
2024-06-25 20:42:18 -0300

    adding edit message feature

--- costs/src/components/pages/Project.js
+++ costs/src/components/pages/Project.js
@@ -5,6 +5,7 @@ import {useState, useEffect} from 'react'
 
 import Loading from '../layout/Loading'
 import Container from '../layout/Container'
+import Message from '../layout/Message'
 import ProjectForm from '../project/ProjectForm'
 
 function Project() {
@@ -12,6 +13,8 @@ function Project() {
 
     const [project, setProject] = useState([])
     const [showProjectForm, setShowProjectForm] = useState(false)
+    const [message, setMessage] = useState()
+    const [type, setType] = useState()
 
     useEffect(() => {
         setTimeout(() => {
@@ -31,7 +34,9 @@ function Project() {
 
     function editPost(project) {
         if(project.budget < project.cost) {
-            // mensagem
+           setMessage('O orçamento não pode ser menor que o custo do projeto.')
+           setType('error')
+           return false
         }
 
         fetch(`http://localhost:5000/projects/${project.id}`, {
@@ -45,7 +50,8 @@ function Project() {
         .then((data) => {
             setProject(data)
             setShowProjectForm(false)
-            // mensagem
+            setMessage('Projeto atualizado!')
+           setType('success')
         })
     }
 
@@ -58,6 +64,7 @@ function Project() {
         {project.name ? (
             <div className={styles.project_details}>
                 <Container customClass="column">
+                    {message && <Message type={type} msg={message} />}
                     <div className={styles.details_container}>
                         <h1>Projeto: {project.name}</h1>
                         <button className={styles.btn} onClick={toggleProjectForm}>

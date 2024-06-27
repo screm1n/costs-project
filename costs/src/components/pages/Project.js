fc33bf42a2d1326cf486a4a6f42313a9e9aa1880
Octávio <screminoctavio@gmail.com>
2024-06-26 22:17:26 -0300

    correcting message bug

--- costs/src/components/pages/Project.js
+++ costs/src/components/pages/Project.js
@@ -33,6 +33,8 @@ function Project() {
     }, [id])
 
     function editPost(project) {
+        setMessage('')
+        
         if(project.budget < project.cost) {
            setMessage('O orçamento não pode ser menor que o custo do projeto.')
            setType('error')

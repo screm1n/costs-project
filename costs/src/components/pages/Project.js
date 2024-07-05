148b172b920ef8fe57b0ae1c73260d082f550c86
Octávio <screminoctavio@gmail.com>
2024-07-05 19:18:34 -0300

    applying services functionalities

--- costs/src/components/pages/Project.js
+++ costs/src/components/pages/Project.js
@@ -10,11 +10,13 @@ import Container from '../layout/Container'
 import Message from '../layout/Message'
 import ProjectForm from '../project/ProjectForm'
 import ServiceForm from '../service/ServiceForm'
+import ServiceCard from '../service/ServiceCard'
 
 function Project() {
     const { id } = useParams()
 
     const [project, setProject] = useState([])
+    const [services, setServices] = useState([])
     const [showProjectForm, setShowProjectForm] = useState(false)
     const [showServiceForm, setShowServiceForm] = useState(false)
     const [message, setMessage] = useState()
@@ -31,6 +33,7 @@ function Project() {
         .then(resp => resp.json())
         .then((data) => {
             setProject(data)
+            setServices(data.services)
         })
         .catch(err => console.log)
         }, 500)
@@ -93,7 +96,7 @@ function Project() {
             body: JSON.stringify(project)
         }).then((resp) => resp.json())
         .then((data) => {
-            // exibir os serviços
+            setShowServiceForm(false)
         })
         .catch(err => console.log(err))
 
@@ -105,6 +108,10 @@ function Project() {
 
     function toggleServiceForm () {
         setShowServiceForm(!showServiceForm)
+    }
+
+    function removeService() {
+
     }
 
     return (
@@ -130,14 +137,14 @@ function Project() {
                                     <span>Valor total utilizado:</span> R${project.cost}
                                 </p>
                                 
-                                
                             </div>
                         ) : (
                             <div className={styles.project_info}>
                                <ProjectForm 
                                handleSubmit={editPost}
                                btnText="Concluir edição" 
-                               projectData={project} />
+                               projectData={project} 
+                            />
                             </div>
                         )}
                     </div>
@@ -157,7 +164,19 @@ function Project() {
                     </div>
                     <h2>Serviços</h2>
                     <Container customClass="start">
-                            <p>Itens de serviços</p>
+                        {services.length > 0 &&
+                            services.map((service) => (
+                                <ServiceCard 
+                                    id={service.id}
+                                    name={service.name}
+                                    cost={service.cost}
+                                    description={service.description}
+                                    key={service.key}
+                                    handleRemove={removeService}
+                                />
+                            ))
+                        }
+                        {services.length === 0 && <p>Não há serviços cadastrados.</p>}
                     </Container>
                 </Container>
             </div>
